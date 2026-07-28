1f27235abf12404b0502096f27678a603c13dbf4
haksoz <59022040+haksoz@users.noreply.github.com>
2026-07-28 18:02:20 +0300

    hesaba tahsilat eklerken canlıdaki sorn düzeltmesi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Support\CurrentApartment;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::composer('layouts.app', function ($view) {
             $availableApartments = collect();
             $currentApartment = null;
